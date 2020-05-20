0513d9ce90e50a118325e14b26f93792c672fb59
ygson <ygson@cookapps.com>
2020-05-21 02:48:34 +0900

    모듈 코드 업로드

---
+++ GoogleSpreadSheetCSVReader.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace GoogleSpreadSheetCSVReader
+{
+    public class SpreadSheetCSVReader
+    {
+        private const string baseUrl = "https://docs.google.com/spreadsheets/d/e/{0}/pub?gid={1}&single=true&output=csv";
+
+        public static void GetSpreadSheetData<T>(string key, string gid, System.Action<T[]> callback)
+        {
+            StaticCoroutine.DoCoroutine(GetDataCoroutine(key, gid, callback));
+        }
+
+        static IEnumerator GetDataCoroutine<T>(string key, string gid, System.Action<T[]> callback)
+        {
+            string url = string.Format(baseUrl, key, gid);
+
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+            {
+                yield return webRequest.SendWebRequest();
+                if (webRequest.isNetworkError)
+                {
+                    Debug.Log("Download Error : " + webRequest.error);
+                    callback?.Invoke(null);
+                }
+                else
+                {
+                    Debug.Log("Data : " + webRequest.downloadHandler.text);
+
+                    T[] data = CSVSerializer.Deserialize<T>(webRequest.downloadHandler.text);
+                    callback?.Invoke(data);
+                }
+            }
+        }
+
+    }
+}
+
